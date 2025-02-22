3717d7db78015e244881e4d99ac1617536e4b036
zeff2421 <191776689+zeff2421@users.noreply.github.com>
2025-02-22 16:39:58 +0200

    Create a function which return a 1 of 3 random string

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return "rock";
+    } else if (choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
